a5fa0fbe53b9149714dac3bf6b2eaccb34cbce55
GildoUser <gildoUser@gmail.com>
2023-12-22 19:43:00 -0300

    test files

---
+++ reed.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+x = ''
+readd = open('txt.txt','r')
+x = readd.read()
+readd.close()
+print(x)
